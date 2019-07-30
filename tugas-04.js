c680157cd1e2fb66ff2456f5e50955ffcbb79c94
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-07-30 23:12:19 +0700

    tugas_04_js
    tugas sudah ok !!!

---
+++ tugas-04.js
@@ -0,0 +1,25 @@
+function tinggi(){
+
+  var a = 160;
+  var b = 170;
+  var c = 180;
+
+
+if(c > b) {
+  console.log("Siswa C Tinggi badannya 180");
+}else{
+  console.log("Siswa B Tinggi badannya 170");
+}
+if (b > a) {
+  console.log("Siswa B Tinggi badannya 170");
+}else {
+  console.log("Siswa A Tinggi badannya 160");
+}
+if (c > a) {
+  console.log("Siswa A Tinggi badannya 160");
+}else {
+  console.log("Siswa C Tinggi badannya 180");
+}
+}
+
+tinggi()
